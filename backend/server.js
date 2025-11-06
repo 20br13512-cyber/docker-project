d2db44819da11d60ac929f7732a5e8d4fdb160ed
aswin us <20br13512@rit.ac.in>
2025-11-06 14:10:31 +0530

    ready for render deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose
 
 // Simple API endpoint
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend!" });
+  res.json({ message: "Hello from backend ,nice work" });
 });
 
 // Start server
